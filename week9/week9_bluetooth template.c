ca95e750c3877b0a62082cbdf075fa199998bc42
Huch0 <heochiyeong@gmail.com>
2024-10-31 19:10:17 +0900

    week9: code

--- week9/week9_bluetooth template.c
+++ week9/week9_bluetooth template.c
@@ -15,75 +15,105 @@ void USART2_Init(void);
 void NVIC_Configure(void);
 
 void RCC_Configure(void)
-{  
+{
     // TODO: Enable the APB2 peripheral clock using the function 'RCC_APB2PeriphClockCmd'
-	
-	/* USART1, USART2 TX/RX port clock enable */
-	
-	/* USART1, USART2 clock enable */
-		
-	/* Alternate Function IO clock enable */
+
+    /* USART1, USART2 TX/RX port clock enable */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+    /* USART1, USART2 clock enable */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+
+    /* Alternate Function IO clock enable */
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 }
 
 void GPIO_Configure(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
-    
-   	// TODO: Initialize the GPIO pins using the structure 'GPIO_InitTypeDef' and the function 'GPIO_Init'
+
+    // TODO: Initialize the GPIO pins using the structure 'GPIO_InitTypeDef' and the function 'GPIO_Init'
 
     /* USART1 pin setting */
-    //TX
-    
-	//RX
-    
+    // TX (PA9)
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    // RX (PA10)
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
     /* USART2 pin setting */
-    //TX
-    
-	//RX
+    // TX (PA2)
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    // RX (PA3)
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
 
 void USART1_Init(void)
 {
     USART_InitTypeDef USART1_InitStructure;
 
-	// Enable the USART1 peripheral
-	USART_Cmd(USART1, ENABLE);
-	
-	// TODO: Initialize the USART using the structure 'USART_InitTypeDef' and the function 'USART_Init'
-	
-	
-	// TODO: Enable the USART1 RX interrupts using the function 'USART_ITConfig' and the argument value 'Receive Data register not empty interrupt'
-	
+    // Enable the USART1 peripheral
+    USART_Cmd(USART1, ENABLE);
+
+    // TODO: Initialize the USART using the structure 'USART_InitTypeDef' and the function 'USART_Init'
+    USART1_InitStructure.USART_BaudRate = 9600;
+    USART1_InitStructure.USART_StopBits = USART_StopBits_1_5;
+    USART1_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART1_InitStructure.USART_Parity = USART_Parity_No;
+    USART1_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART1_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    USART_Init(USART1, &USART1_InitStructure);
+
+    // TODO: Enable the USART1 RX interrupts using the function 'USART_ITConfig' and the argument value 'Receive Data register not empty interrupt'
+    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
 }
 
 void USART2_Init(void)
 {
     USART_InitTypeDef USART2_InitStructure;
 
-	// Enable the USART2 peripheral
-	USART_Cmd(USART2, ENABLE);
-	
-	// TODO: Initialize the USART using the structure 'USART_InitTypeDef' and the function 'USART_Init'
-	
-	
-	// TODO: Enable the USART2 RX interrupts using the function 'USART_ITConfig' and the argument value 'Receive Data register not empty interrupt'
-	
+    // Enable the USART2 peripheral
+    USART_Cmd(USART2, ENABLE);
+
+    // TODO: Initialize the USART using the structure 'USART_InitTypeDef' and the function 'USART_Init'
+    USART2_InitStructure.USART_BaudRate = 9600;
+    USART2_InitStructure.USART_StopBits = USART_StopBits_1_5;
+    USART2_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART2_InitStructure.USART_Parity = USART_Parity_No;
+    USART2_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART2_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    USART_Init(USART2, &USART2_InitStructure);
+
+    // TODO: Enable the USART2 RX interrupts using the function 'USART_ITConfig' and the argument value 'Receive Data register not empty interrupt'
+    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
 }
 
-void NVIC_Configure(void) {
+void NVIC_Configure(void)
+{
 
     NVIC_InitTypeDef NVIC_InitStructure;
-	
+
     // TODO: fill the arg you want
-    NVIC_PriorityGroupConfig(???);
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
 
     // USART1
     // 'NVIC_EnableIRQ' is only required for USART setting
     NVIC_EnableIRQ(USART1_IRQn);
     NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = ; // TODO
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = ; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 3; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;        // TODO
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
 
@@ -91,56 +121,65 @@ void NVIC_Configure(void) {
     // 'NVIC_EnableIRQ' is only required for USART setting
     NVIC_EnableIRQ(USART2_IRQn);
     NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = ; // TODO
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = ; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 3; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;        // TODO
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
 }
 
-void USART1_IRQHandler() {
+void USART1_IRQHandler()
+{
     uint16_t word;
-    if(USART_GetITStatus(USART1,USART_IT_RXNE)!=RESET){
+    if (USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
+    {
         // the most recent received data by the USART1 peripheral
         word = USART_ReceiveData(USART1);
 
-        // TODO implement
+        // TODO implement ???
+        while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+        USART_SendData(USART2, word);
 
         // clear 'Read data register not empty' flag
-    	USART_ClearITPendingBit(USART1,USART_IT_RXNE);
+        USART_ClearITPendingBit(USART1, USART_IT_RXNE);
     }
 }
 
-void USART2_IRQHandler() {
+void USART2_IRQHandler()
+{
     uint16_t word;
-    if(USART_GetITStatus(USART2,USART_IT_RXNE)!=RESET){
+    if (USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)
+    {
         // the most recent received data by the USART2 peripheral
         word = USART_ReceiveData(USART2);
 
         // TODO implement
+        while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+        USART_SendData(USART1, word);
 
         // clear 'Read data register not empty' flag
-    	USART_ClearITPendingBit(USART2,USART_IT_RXNE);
+        USART_ClearITPendingBit(USART2, USART_IT_RXNE);
     }
 }
 
 int main(void)
 {
-  char msg[] = "abcde\r\n";
-  unsigned int i;
-  
+    char msg[] = "abcde\r\n";
+    unsigned int i;
+
     SystemInit();
 
     RCC_Configure();
 
     GPIO_Configure();
 
-    USART1_Init();      // pc
-    
-    USART2_Init();      // bluetooth
+    USART1_Init(); // pc
+
+    USART2_Init(); // bluetooth
 
     NVIC_Configure();
 
-    while (1) {
+    while (1)
+    {
     }
     return 0;
 }
